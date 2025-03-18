6f8c95000ae7b52dccb9f54ed209a58380c579d2
Fernando Acauan <mcflyj038@gmail.com>
2025-03-18 16:11:23 -0300

    hora do show

--- scripts.js
+++ scripts.js
@@ -8,14 +8,21 @@
 
 
 ///////////////////////////////////////////////////////////////////////////////
-// Fetch os dados da APIs
+// Name: GetRates()
+// Desc: Fetch data.
 ///////////////////////////////////////////////////////////////////////////////
-
-async function getRates() {
-    let data = await fetch("https://v6.exchangerate-api.com/v6/7e3d8fa49ac7bfa926531550/latest/USD");
+async function GetRates() 
+{
+    const kCoin = document.getElementById("textoConvertido").value.toLower();
+    let data = await fetch(`https://v6.exchangerate-api.com/v6/7e3d8fa49ac7bfa926531550/latest/${kCoin}`);
 
     if(!data.ok)
-        return;
+    {
+        throw new Error("Could not fetch request");
+    }
+        
 
     console.log(data.json());
 }
+
+document.getElementById("textoConvertido").innerHTML = data;
