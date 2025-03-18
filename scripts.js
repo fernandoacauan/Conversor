c63a7130014fb37d63f868f604bdb52e6beb99f2
Fernando Acauan <mcflyj038@gmail.com>
2025-03-18 15:43:03 -0300

    .

--- scripts.js
+++ scripts.js
@@ -8,7 +8,9 @@
 
 
 
-
+///////////////////////////////////////////////////////////////////////////////
+// Fetch os dados da APIs
+///////////////////////////////////////////////////////////////////////////////
 fetch("https://v6.exchangerate-api.com/v6/7e3d8fa49ac7bfa926531550/latest/USD")
     .then(response => console.log(response))
-    .catch(error => console.error(error));
+     .catch(error => console.error(error));
