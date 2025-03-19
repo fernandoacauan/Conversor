75682933a5e624003d466ffe65cff92bfda18cef
Fernando Acauan <mcflyj038@gmail.com>
2025-03-19 15:08:22 -0300

    meudeus

--- scripts.js
+++ scripts.js
@@ -11,18 +11,29 @@
 // Name: GetRates()
 // Desc: Fetch data.
 ///////////////////////////////////////////////////////////////////////////////
-async function GetRates() 
+window.onload = async function()
 {
-    const kCoin = document.getElementById("textoConvertido").value.toLower();
-    let data = await fetch(`https://v6.exchangerate-api.com/v6/7e3d8fa49ac7bfa926531550/latest/${kCoin}`);
-
+    let data = await fetch('https://v6.exchangerate-api.com/v6/7e3d8fa49ac7bfa926531550/latest/USD');
     if(!data.ok)
     {
-        throw new Error("Could not fetch request");
+       throw new Error("Could not fetch request");
     }
+    let jsonData = await data.json();
+    let paises = Object.keys(jsonData.conversion_rates);
+
+    var select = document.getElementById('opcao');
+    
         
+    paises.forEach(pais => {
+        var option = document.createElement('option');
+        option.text = option.value = pais;
+        select.add(option, pais);
+
+        console.log(pais);
+        
+    });
 
     console.log(data.json());
-}
 
-document.getElementById("textoConvertido").innerHTML = data;
+
+}
